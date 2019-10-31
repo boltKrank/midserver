746682c46eb389968d67a56d1f5876ac9e1fe306
Simon Anderson <simon.anderson111@gmail.com>
2019-10-31 16:04:13 +1100

    Added download and extract

---
+++ manifests/init.pp
@@ -0,0 +1,40 @@
+# @summary A short summary of the purpose of this class
+#
+# Configures and installs a ServiceNow mid server
+#
+# @example
+#   include midserver
+class midserver (
+  $installer_url = 'https://install.service-now.com/glide/distribution/builds/package/mid/2019/09/24/mid.newyork-06-26-2019__patch2-09-18-2019_09-24-2019_1701.linux.x86-64.zip',
+){
+
+  #Dependency for puppet-archive:
+  if $facts['osfamily'] != 'windows' {
+    package { 'wget':
+      ensure => present,
+    }
+
+    package { 'bunzip':
+      ensure => present,
+    }
+
+    Archive {
+      provider => 'wget',
+      require  => Package['wget', 'bunzip'],
+    }
+  }
+
+  #Make install directory
+  file { ['/servicenow','/servicenow/midserver']:
+    ensure => directory,
+  }
+
+  #Download and extract package (mod 'puppet-archive', '4.3.0')
+  archive { '/servicenow/midserver.zip' :
+    source       => $installer_url,
+    extract      => true,
+    extract_path => '/servicenow',
+    creates      => '/servicenow/midserver',
+  }
+
+}
