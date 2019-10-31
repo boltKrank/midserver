e7a4bffc8ec9160a1bb7cfbe75c6de90f894598c
Simon Anderson <simon.anderson111@gmail.com>
2019-11-01 10:13:37 +1100

    Removed wget and bunzip

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,23 +8,6 @@ class midserver (
   $installer_url = 'https://install.service-now.com/glide/distribution/builds/package/mid/2019/09/24/mid.newyork-06-26-2019__patch2-09-18-2019_09-24-2019_1701.linux.x86-64.zip',
 ){
 
-  #Dependency for puppet-archive:
-  if $facts['osfamily'] != 'windows' {
-    package { 'wget':
-      ensure => present,
-    }
-
-    #package { 'bunzip':
-    #  ensure => present,
-    #}
-
-    Archive {
-      provider => 'wget',
-      require  => Package['wget'],
-      #require  => Package['wget', 'bunzip'],
-    }
-  }
-
   #Make install directory
   file { ['/servicenow','/servicenow/midserver']:
     ensure => directory,
@@ -37,6 +20,5 @@ class midserver (
     extract_path => '/servicenow',
     creates      => '/servicenow/midserver',
   }
-
 
 }
