a20134ffcdacc02638c9c91d0cfc2fd39936e7aa
Simon Anderson <simon.anderson111@gmail.com>
2019-11-01 15:27:57 +1100

    Updated template

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,10 @@
 #
 # @example
 #   include midserver
-class midserver {
+class midserver (
+  $midserver_installer = '/servicenow/midserver/midserver.zip',
+  $installer_url = 'https://install.service-now.com/glide/distribution/builds/package/mid/2019/09/24/mid.newyork-06-26-2019__patch2-09-18-2019_09-24-2019_1701.linux.x86-64.zip'
+){
 
   #Make install directory
   file { ['/servicenow','/servicenow/midserver']:
@@ -14,17 +17,17 @@ class midserver {
   # unzip package (pre-req for archive module on posix)
   package { 'unzip':
     ensure => 'present',
-    before => Archive['/servicenow/midserver/midserver.zip'],
+    before => Archive[$midserver_installer],
   }
 
   #Download and extract package (mod 'puppet-archive', '4.3.0')
-  archive { '/servicenow/midserver/midserver.zip':
+  archive { $midserver_installer:
     ensure       => present,
     extract      => true,
-    extract_path => '/servicenow/midserver',
+    extract_path => $midserver_installer,
     cleanup      => false,
-    creates      => '/servicenow/midserver/midserver.zip',
-    source       => 'https://install.service-now.com/glide/distribution/builds/package/mid/2019/09/24/mid.newyork-06-26-2019__patch2-09-18-2019_09-24-2019_1701.linux.x86-64.zip',
+    creates      => $midserver_installer,
+    source       => $installer_url,
     require      => File['/servicenow/midserver'],
   }
 
